0cdcd41d4094665c8ab2200e47c6b70cd1d1de3b
HuyTran0996 <tranviquanghuy@gmail.com>
2024-05-11 10:53:48 +0700

    2024-05-11 fix bug with Navbar.scss @media only work correctly if add !important

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+import "./SearchForm.scss";
+
+const SearchForm = () => {
+  return <div>SearchForm</div>;
+};
+
+export default SearchForm;
